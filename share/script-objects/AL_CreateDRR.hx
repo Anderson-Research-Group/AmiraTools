41022914e5bfb43ef8af7240a524e522ccaf5fd3
Lindsay Schuring <Ontario@AA-Ontario.ad.utah.edu>
2025-02-26 14:10:59 -0700

    added ability of hx script to remove all intermediate modules. Also hides all intermediate modules from user view. TODO - need to address issue of canceling script midway through run

--- share/script-objects/AL_CreateDRR.hx
+++ share/script-objects/AL_CreateDRR.hx
@@ -1,79 +1,87 @@
 # Surface Generation for Amira 5.6, 6.0.1, and 6.2.0
 # L. Schuring, 10/2024
-# Updates:
-# - S. Kussow, 12/4/2024: removed 1 slice enlargement after crop, revised STL file path
-
-# ESTABLISH USER VARIABLES
+# Required Variables for script to run. These variables are defined in the AL_SurfGen.scro compute procedure:
+	# AMIRA_LOCAL
+	# outputDir 
 
 # Scan Surface To Volume
-create HxScanConvertSurface "Scan Surface To Volume"
-"Scan Surface To Volume" setVar "CustomHelp" {HxScanConvertSurface}
-"Scan Surface To Volume" data connect "$surfFile"
-"Scan Surface To Volume" field connect "$Dataset"
-"Scan Surface To Volume" fire
-[ {Scan Surface To Volume} create result ] setLabel "$surfFile.scanConverted"
+set AL_ss2v [create HxScanConvertSurface "Scan Surface To Volume"]
+$AL_ss2v setVar "CustomHelp" {HxScanConvertSurface}
+$AL_ss2v data connect "$surfFile"
+$AL_ss2v field connect "$Dataset"
+$AL_ss2v fire
+set surfFile_scanConverted [ $AL_ss2v create result ] 
 
 # CONVERT IMAGE TYPE TO 8-BIT UNSIGNED
 	# "CastField1" outputType setIndex 0 0 is the command for 8-bit unsigned.  If the order of the list ever changes, where 8-bit unsigned is 3rd on the drop-down menu (index=2_ , then the command would be: "CastField1" outputType setIndex 0 0
-create HxCastField "CastField1"
-"CastField1" setVar "CustomHelp" {HxCastField}
-"CastField1" data connect "$Dataset" 
-"CastField1" outputType setIndex 0 0
-"CastField1" scaling setValue 0 0.0625
-"CastField1" scaling setValue 1 1024
-"CastField1" voxelGridOptions setValue 0 1
-"CastField1" fire
-[ {CastField1} create result ] setLabel "dicom_1x.to-byte"
+set AL_castfield [create HxCastField "CastField1"]
+$AL_castfield setVar "CustomHelp" {HxCastField}
+$AL_castfield data connect "$Dataset" 
+$AL_castfield outputType setIndex 0 0
+$AL_castfield scaling setValue 0 0.0625
+$AL_castfield scaling setValue 1 1024
+$AL_castfield voxelGridOptions setValue 0 1
+$AL_castfield fire
+set data1x_tobyte [ $AL_castfield create result ] 
 
-# ARITHMETIC
-create HxArithmetic "Arithmetic"
-"Arithmetic" setVar "CustomHelp" {HxArithmetic}
-"Arithmetic" inputA connect "dicom_1x.to-byte"
-"Arithmetic" inputB connect "$surfFile.scanConverted"
-"Arithmetic" inputC disconnect
-"Arithmetic" fire
-"Arithmetic" resultChannels setIndex 0 0
-"Arithmetic" expr0 setState {A*(B>0)}
-"Arithmetic" resultType setValue 1
-"Arithmetic" resultLocation setValue 0
+# ARITHMETIC - defining what gets zeroed out from image data based on whether or not it is in or out of label 
+set AL_arithmetic [create HxArithmetic "Arithmetic"]
+$AL_arithmetic setVar "CustomHelp" {HxArithmetic}
+$AL_arithmetic inputA connect $data1x_tobyte
+$AL_arithmetic inputB connect $surfFile_scanConverted
+$AL_arithmetic inputC disconnect
+$AL_arithmetic fire
+$AL_arithmetic resultChannels setIndex 0 0
+$AL_arithmetic expr0 setState {A*(B>0)}
+$AL_arithmetic resultType setValue 1
+$AL_arithmetic resultLocation setValue 0
+
 # get resolution from DICOM image stack
 set dicomDims ["$Dataset" getDims]
-"Arithmetic" resolution setValue 0 [lindex $dicomDims 0]
-"Arithmetic" resolution setValue 1 [lindex $dicomDims 1]
-"Arithmetic" resolution setValue 2 [lindex $dicomDims 2]
-"Arithmetic" fire
-[ {Arithmetic} create result ] setLabel "Result"
+$AL_arithmetic resolution setValue 0 [lindex $dicomDims 0]
+$AL_arithmetic resolution setValue 1 [lindex $dicomDims 1]
+$AL_arithmetic resolution setValue 2 [lindex $dicomDims 2]
+$AL_arithmetic fire
+set AL_arithmeticResult [$AL_arithmetic create result ]
 
 # AUTO-CROPPING cropping with threshold set to 1 (anything smaller than 1 will be outside of crop bounds), ie cropping to last slice WITH data
-set "cropBounds" ["Result" crop -auto 1]
+set "cropBounds" [$AL_arithmeticResult crop -auto 1]
 
 # Attach bounding box so that it's easier to get bounding box values to write to file
-create HxBoundingBox "BoundingBox"
-"BoundingBox" data connect "Result"
-"BoundingBox" select
-set lowerBound ["BoundingBox" LowerLeft getValue]
-set upperBound ["BoundingBox" UpperRight getValue]
-"Result" select
-set voxelSize ["Result" Voxelsize getValue]
+set AL_boundingbox [create HxBoundingBox "BoundingBox"]
+$AL_boundingbox data connect $AL_arithmeticResult
+$AL_boundingbox select
+set lowerBound [$AL_boundingbox LowerLeft getValue]
+set upperBound [$AL_boundingbox UpperRight getValue]
+$AL_arithmeticResult select
+set voxelSize [$AL_arithmeticResult Voxelsize getValue]
 
 # Conver to 16-bit
-create HxCastField "CastField2"
-"CastField2" setVar "CustomHelp" {HxCastField}
-"CastField2" data connect "Result" 
-"CastField2" outputType setIndex 0 2
-"CastField2" scaling setValue 0 1
-"CastField2" scaling setValue 1 0
-"CastField2" fire
-[ {CastField2} create result ] setLabel "finalDRR"
+set AL_castfield2 [create HxCastField "CastField2"]
+$AL_castfield2 setVar "CustomHelp" {HxCastField}
+$AL_castfield2 data connect $AL_arithmeticResult 
+$AL_castfield2 outputType setIndex 0 2
+$AL_castfield2 scaling setValue 0 1
+$AL_castfield2 scaling setValue 1 0
+$AL_castfield2 fire
+set DRRname "[file rootname [file tail $surfFile]]_DRR"
+[$AL_castfield2 create result ] setLabel "$DRRname"
+"$DRRname" showIcon
 
 # If output directory given, export surface as stl file, save in script directory
 if {[info exist outputDir] & [llength $outputDir]> 0} {
-	file mkdir "$outputDir/for_mtwtesla/"
+	# create an output file name based on surfFile 
 	set outputFileName [file rootname [file tail $surfFile]]
-	"finalDRR" exportData "3D Tiff" "$outputDir/for_mtwtesla/$outputFileName.tiff"
 	
-	# Save cropping info to txt file
+	#create directory to save output files 'for_mtwtesla'
+	file mkdir "$outputDir/for_mtwtesla/"
+
+	# export tiff file and txt file with cropping info
+	"$DRRname" exportData "3D Tiff" "$outputDir/for_mtwtesla/$outputFileName.tiff"
 	set fileId [open "$outputDir/for_mtwtesla/$outputFileName.txt" "w"]
 	puts $fileId " Cropping Bounds (xmin,xmax,ymin,ymax,zmin,zmax): $cropBounds \n Bounding Box (xmin,ymin,zmin,xmax,ymax,zmax): $lowerBound $upperBound \n Voxel size (x,y,z): $voxelSize"
 	close $fileId
 }
+
+# REMOVE INTERMEDIATE MODULES
+remove $AL_ss2v $surfFile_scanConverted $AL_castfield $data1x_tobyte $AL_arithmetic $AL_arithmeticResult $AL_boundingbox $AL_castfield2
