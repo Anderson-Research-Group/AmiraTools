0e589b097db4fcadd8843c07aa1060b6df450c7e
Lindsay Schuring <Ontario@AA-Ontario.ad.utah.edu>
2025-03-06 17:33:02 -0700

    First draft of AutoAnkleLabel. Takes Seg3D data, reformats bounding box to fit dicom data and creates labels specific to Ankle project

---
+++ share/script-objects/AL_AutoAnkleLabels.hx
@@ -0,0 +1,36 @@
+# Auto Ankle Label Generation. Tested on Amira 6.0.1
+# L. Schuring, 3/2025
+	
+set dataTif "Subj01.tiff"	
+set dataDicom "Subj01"
+
+# replace Tif bbox info with Dicom bbox info
+set bboxDicom [$dataDicom getBoundingBox]
+$dataTif setBoundingBox {*}$bboxDicom 
+	#For troubleshooting: 
+	#set bboxTif [$dataTif getBoundingBox]
+
+# Create a multi-thresholding tool to auto segment Tif stack 
+set labelGen [create HxLabelVoxel]
+$labelGen data connect $dataTif
+$labelGen regions setValue "Exterior Tibia Calcaneus Talus"
+$labelGen fire
+$labelGen boundary01 setValue 0
+$labelGen boundary12 setValue 1
+$labelGen boundary23 setValue 2
+$labelGen applyTransformToResult 1
+set labelTib [[$labelGen create] setLabel "$dataDicom-Tibia"]
+set labelCal [[$labelTib duplicate] setLabel "$dataDicom-Calcaneus"]
+set labelTal [[$labelTib duplicate] setLabel "$dataDicom-Talus"]
+
+# remove materials from each label so only the material of interest is in the label
+$labelTib removeMaterial "Calcaneus"
+$labelTib removeMaterial "Talus"
+$labelCal removeMaterial "Tibia"
+$labelCal removeMaterial "Talus"
+$labelTal removeMaterial "Calcaneus"
+$labelTal removeMaterial "Tibia"
+
+# Each label now has all three materials
+
+
