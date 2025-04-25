d9beda0ef264752659e8dfbe1608fab09ce04e34
Lindsay Schuring <Ontario@AA-Ontario.ad.utah.edu>
2025-04-25 12:05:50 -0600

    added BatchExport.hx script. WIP, could be made to be more modular

---
+++ share/script-objects/BatchExport.hx
@@ -0,0 +1,26 @@
+# Surface Generation for Amira 5.6, 6.0.1, and 6.2.0
+# L. Schuring, 10/2024
+
+# ESTABLISH USER VARIABLES
+remove -all
+# EDIT BELOW LINE: to reflect the file type you'd like to import
+set importList [glob -directory $SCRIPTDIR -tails *.ply]
+file mkdir "$SCRIPTDIR/Export/"
+echo $importList
+
+# Set units if not already set
+if {[_units isUnitsManagementActivated] == 1} {
+    _units setDisplayCoordinatesUnit mm
+    _units setWorkingCoordinatesUnit mm}
+
+# For loop to open each file and export as different file t
+foreach fileName $importList {
+	# Load File
+	set hideNewModules 1
+	[load -unit mm $SCRIPTDIR/$fileName] setLabel $fileName
+	
+	# EDIT BELOW LINE: to reflect the file type you'd like your export to be in
+	$fileName exportData "STL ascii" "$SCRIPTDIR/Export/$fileName.stl"
+	
+	remove $fileName
+}
