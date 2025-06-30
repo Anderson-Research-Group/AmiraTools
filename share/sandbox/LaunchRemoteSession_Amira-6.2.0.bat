1ca35f9a3b7641dcdd0f7d3df7cdca4f72ca3cd1
leaschuring <>
2025-06-30 16:23:33 -0600

    separated LaunchRemoteSession bat script into two by Amira executable that is launched

---
+++ share/sandbox/LaunchRemoteSession_Amira-6.2.0.bat
@@ -0,0 +1,7 @@
+set SESSION_ID=unknown
+for /f "tokens=3-4" %%a in ('query session %username%') do @if "%%b"=="Active" set SESSION_ID=%%a
+tscon %SESSION_ID% /dest:console
+start "" "C:\Program Files\Amira-6.2.0\bin\arch-Win64VC12-Optimize\Amira.exe"
+timeout /t 6
+rundll32.exe user32.dll,LockWorkStation
+
